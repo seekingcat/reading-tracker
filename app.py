8b4b680e97acd76543e31821da78adf6ceec5994
Melanie Hudson <llana_a154@yahoo.co.uk>
2026-02-22 12:49:04 +0000

    added various view templates and links from homepage to add title page

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def tracker():
     return render_template('tracker.html')
 
+@app.route('/add')
+def addtitle():
+    return render_template('addtitle.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
