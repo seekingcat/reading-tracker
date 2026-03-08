879adfe71f5c36231cdc01ef4a41515b12856a00
Melanie Hudson <llana_a154@yahoo.co.uk>
2026-03-08 13:14:46 +0000

    fixing functionality of view page

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, redirect, url_for
+from database import get_all_books, add_book, init_db
+init_db()
 
 app = Flask(__name__)
 
@@ -12,7 +14,9 @@ def addtitle():
 
 @app.route('/view')
 def viewtitle():
-    return render_template('viewtitle.html')
+    books = get_all_books()
+
+    return render_template('viewtitle.html', books = books)
 
 if __name__ == '__main__':
     app.run(debug=True)
