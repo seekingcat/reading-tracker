5e38870662b48ecc45303ab484c1994af3f25055
Melanie Hudson <llana_a154@yahoo.co.uk>
2026-03-08 13:48:27 +0000

    add route completed

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 from database import get_all_books, add_book, init_db
 init_db()
 
@@ -8,8 +8,8 @@ app = Flask(__name__)
 def tracker():
     return render_template('tracker.html')
 
-@app.route('/add')
-def addtitle():
+@app.route('/addtitle')
+def addtitle_page():
     return render_template('addtitle.html')
 
 @app.route('/view')
@@ -17,6 +17,17 @@ def viewtitle():
     books = get_all_books()
 
     return render_template('viewtitle.html', books = books)
+
+
+@app.route('/add', methods=['POST'])
+def addtitle():
+    title = request.form.get('title')
+    author = request.form.get('author')
+    status = request.form.get('status')
+
+    add_book(title, author, status)
+    return redirect(url_for('viewtitle'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
